9092c099fdd1cefc97c5fe07ec47a494687bf4ef
Pei Wang <wangpei0068@gmail.com>
2013-10-26 18:19:22 -0700

    delete syntax files

--- WhitespaceDependency.rb
+++
@@ -1,6 +0,0 @@
-def square(a)
-	return a ** 2
-end
-
-puts square(3+2)+1
-puts square (3+2)+1
--- testBracket.rb
+++
@@ -1,9 +0,0 @@
-def interestingMethod x
-	puts x[0] == 1 ? "yes" : "no"
-end
-
-puts "test integer"
-1.upto(20) {|x| interestingMethod x}
-
-puts "test array"
-interestingMethod [1, 2, 3]
